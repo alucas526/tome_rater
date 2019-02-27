d28ff00b9e734effb2d567268a1cc66d8f952e7c
Alan Lucas <alucas@gmail.com>
2019-02-26 21:00:00 -0500

    Added TomeRater class and some methods. Still lots to do.

--- TomeRater.py
+++ TomeRater.py
@@ -39,8 +39,7 @@ class User(object):
                     total_ratings += rating
                     rated += 1
                 avg_rating = total_ratings / rated
-            return "{name} has read {n} books and rated {rated} of them, for an average book rating of {avg_rating}".format(name=self.name, n=len(self.books), rated=rated, avg_rating=avg_rating)
-
+            return "{name} has read {n} book(s) and rated {rated} of them, for an average book rating of {avg_rating}".format(name=self.name, n=len(self.books), rated=rated, avg_rating=avg_rating)
 
 
 class Book(object):
@@ -118,30 +117,52 @@ class NonFiction(Book):
         return self.level
 
 
-death_of_a_king = Book("Death Of A King", 9780316332774)
-making_work_visible = Book("Making Work Visible: Exposing Time Theft to Optimize Work & Flow", 9781942788157)
-kav_and_clay = Fiction("The Amazing Adventures of Kavalier & Clay", "Michael Chabon", 9782002400873)
-python_crash_course = NonFiction("Python Crash Course: A Hands-On, Project-Based Introduction to Programming", "Python programming", "beginner", 9781593276034)
-alucas = User("Alan Lucas", "alucas@gmail.com")
-blucas = User("Alan Lucas", "alucas@gmail.com")
-print(alucas)
-print(alucas.__eq__(blucas))
-print(death_of_a_king)
-print(death_of_a_king.add_rating(6))
-print(death_of_a_king.add_rating(3))
-print(death_of_a_king.add_rating(4))
-print(death_of_a_king.add_rating(2))
-print(death_of_a_king.add_rating(3))
-print(death_of_a_king.ratings)
-print(death_of_a_king.get_avg_rating())
-print(making_work_visible.get_avg_rating())
-print(kav_and_clay)
-print(kav_and_clay.get_author())
-print(python_crash_course.get_level())
-print(python_crash_course.get_subject())
-print(python_crash_course)
-print(alucas.read_book(kav_and_clay, 4))
-print(alucas.read_book(python_crash_course, 3))
-print(alucas.read_book(death_of_a_king))
-print(alucas.get_avg_rating())
-print(python_crash_course.get_avg_rating())
+class TomeRater:
+    def __init__(self):
+        self.users = {}
+        self.books = {}
+
+    def __repr__(self):
+        return "Welcome to TomeRater!"
+
+    def create_book(self, title, isbn):
+        return Book(title, isbn)
+
+    def create_novel(self, title, author, isbn):
+        return Fiction(title, author, isbn)
+
+    def create_non_fiction(self, title, subject, level, isbn):
+        return NonFiction(title, subject, level, isbn)
+
+    def add_book_to_user(self, book, email, rating=None):
+        if email in self.users.keys():
+            if User.get_email(email):
+                User.read_book(book, rating)
+                Book.add_rating(book, rating)
+            # if book in self.books:
+        else:
+            return "No user with email address {email}".format(email=email)
+
+    def add_user(self, name, email, books=None):
+        User(name, email)
+        if books is not None:
+            for book in books:
+                self.add_book_to_user(book, email)
+
+
+
+# user1 = User("Bob", "bob@bob.com")
+# book1 = Fiction("It", "Stephen King", 9781501175466)
+# print(user1.read_book(book1, 3.5))
+# print(user1.get_avg_rating())
+# print(book1.get_avg_rating())
+# print(TomeRater().create_book("This", 5645484654))
+# print(TomeRater().create_novel("Hey", "Bob", 243572345897))
+# print(TomeRater().add_book_to_user("It", user1.email))
+# TomeRater().users["bob@bob.com"] = "Bob Smith"
+# print(TomeRater().users)
+# print(TomeRater().add_book_to_user(book1.title, user1.email))
+#
+# xusers = {}
+# xusers["bob@bob.com"] = "Bob Simon"
+# print(xusers)
